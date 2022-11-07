194725160ffd0c3ad2e03affdcc4490d9e498c5b
VasVol <111646443+VasVol@users.noreply.github.com>
2022-11-07 23:28:48 +0300

    Update taskC.cpp

--- 2contest/taskC.cpp
+++ 2contest/taskC.cpp
@@ -2,68 +2,30 @@
 #include <string>
 #include <vector>
 
-struct HeapMin {
-  std::vector<long long> v;
-  std::vector<int> num;
-  std::vector<int> ptr;
-  HeapMin() {
-    v.resize(1);
-    num.resize(1);
-    ptr.resize(1);
-  }
-  void Exchange(int i, int j) {
-    std::swap(v[i], v[j]);
-    std::swap(num[i], num[j]);
-    std::swap(ptr[num[i]], ptr[num[j]]);
-  }
-  void SiftUp(int i) {
-    while (i > 1 && v[i / 2] > v[i]) {
-      Exchange(i, i / 2);
-      i /= 2;
-    }
-  }
-  void SiftDown(int i) {
-    while (2 * i <= (int)v.size() - 1) {
-      int u = 2 * i;
-      if ((2 * i + 1 <= (int)v.size() - 1) && (v[2 * i + 1] < v[u])) {
-        u = 2 * i + 1;
-      }
-      if (v[i] > v[u]) {
-        Exchange(i, u);
-        i = u;
-      } else {
-        break;
-      }
-    }
-  }
-  void Insert(int x, int idx) {
-    v.push_back(x);
-    num.push_back(idx);
-    ptr.push_back((int)v.size() - 1);
-    SiftUp((int)v.size() - 1);
-  }
+class HeapMin {
+ private:
+  std::vector<long long> v_;
+  std::vector<int> num_;
+  std::vector<int> ptr_;
+  void Exchange(int i, int j);
+  void SiftUp(int i);
+  void SiftDown(int i);
+
+ public:
+  HeapMin();
+  void Insert(int x, int idx);
   long long GetMin() {
-    ptr.push_back(0);
-    return v[1];
+    ptr_.push_back(0);
+    return v_[1];
   }
-  void ExtractMin() {
-    Exchange(1, (int)v.size() - 1);
-    v.pop_back();
-    num.pop_back();
-    SiftDown(1);
-    ptr.push_back(0);
-  }
-  void DecreaseKey(int idx, int d) {
-    v[ptr[idx]] -= (long long)d;
-    SiftUp(ptr[idx]);
-    ptr.push_back(0);
-  }
+  void ExtractMin();
+  void DecreaseKey(int idx, long long d);
 };
 
 void FastInputOutput() {
   std::ios_base::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
 }
 
 int main() {
@@ -88,4 +50,60 @@ int main() {
       a.DecreaseKey(i, d);
     }
   }
+}
+
+void HeapMin::Exchange(int i, int j) {
+  std::swap(v_[i], v_[j]);
+  std::swap(num_[i], num_[j]);
+  std::swap(ptr_[num_[i]], ptr_[num_[j]]);
+}
+
+void HeapMin::SiftUp(int i) {
+  while (i > 1 && v_[i / 2] > v_[i]) {
+    Exchange(i, i / 2);
+    i /= 2;
+  }
+}
+
+void HeapMin::SiftDown(int i) {
+  int sz = v_.size();
+  while (2 * i <= sz - 1) {
+    int u = 2 * i;
+    if ((2 * i + 1 <= sz - 1) && (v_[2 * i + 1] < v_[u])) {
+      u = 2 * i + 1;
+    }
+    if (v_[i] > v_[u]) {
+      Exchange(i, u);
+      i = u;
+    } else {
+      break;
+    }
+  }
+}
+
+void HeapMin::Insert(int x, int idx) {
+  v_.push_back(x);
+  num_.push_back(idx);
+  ptr_.push_back(v_.size() - 1);
+  SiftUp(v_.size() - 1);
+}
+
+void HeapMin::ExtractMin() {
+  Exchange(1, v_.size() - 1);
+  v_.pop_back();
+  num_.pop_back();
+  SiftDown(1);
+  ptr_.push_back(0);
+}
+
+void HeapMin::DecreaseKey(int idx, long long d) {
+  v_[ptr_[idx]] -= d;
+  SiftUp(ptr_[idx]);
+  ptr_.push_back(0);
+}
+
+HeapMin::HeapMin() {
+  v_.resize(1);
+  num_.resize(1);
+  ptr_.resize(1);
 }
