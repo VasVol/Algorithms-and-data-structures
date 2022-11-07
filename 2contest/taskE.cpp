5f894967a0850602e8340cee25d0e86bfbc72908
VasVol <111646443+VasVol@users.noreply.github.com>
2022-11-07 23:55:43 +0300

    Update taskE.cpp

--- 2contest/taskE.cpp
+++ 2contest/taskE.cpp
@@ -2,104 +2,60 @@
 #include <string>
 #include <vector>
 
-struct Element {
-  int val;
-  int idx;
-  Element(int val = 0, int idx = 0) {
-    this->val = val;
-    this->idx = idx;
-  }
-};
-
-struct MinMax {
-  std::vector<Element> heap_min;
-  std::vector<Element> heap_max;
-  MinMax() {
-    heap_min.resize(1);
-    heap_max.resize(1);
-  }
+class MinMax {
+ private:
+  struct Element {
+    int val;
+    int idx;
+    Element(int val = 0, int idx = 0) : val(val), idx(idx) {}
+  };
+  std::vector<Element> heap_min_;
+  std::vector<Element> heap_max_;
+  void EraseHeapMin(int i);
+  void EraseHeapMax(int i);
   void ExchangeHeapMin(int i, int j) {
-    std::swap(heap_min[i], heap_min[j]);
-    std::swap(heap_max[heap_min[i].idx].idx, heap_max[heap_min[j].idx].idx);
+    std::swap(heap_min_[i], heap_min_[j]);
+    std::swap(heap_max_[heap_min_[i].idx].idx, heap_max_[heap_min_[j].idx].idx);
   }
   void ExchangeHeapMax(int i, int j) {
-    std::swap(heap_max[i], heap_max[j]);
-    std::swap(heap_min[heap_max[i].idx].idx, heap_min[heap_max[j].idx].idx);
+    std::swap(heap_max_[i], heap_max_[j]);
+    std::swap(heap_min_[heap_max_[i].idx].idx, heap_min_[heap_max_[j].idx].idx);
   }
   void SiftUpHeapMin(int i) {
-    while (i > 1 && heap_min[i / 2].val > heap_min[i].val) {
+    while (i > 1 && heap_min_[i / 2].val > heap_min_[i].val) {
       ExchangeHeapMin(i, i / 2);
       i /= 2;
     }
   }
   void SiftUpHeapMax(int i) {
-    while (i > 1 && heap_max[i / 2].val < heap_max[i].val) {
+    while (i > 1 && heap_max_[i / 2].val < heap_max_[i].val) {
       ExchangeHeapMax(i, i / 2);
       i /= 2;
     }
   }
-  void SiftDownHeapMin(int i) {
-    while (2 * i <= (int)heap_min.size() - 1) {
-      int u = 2 * i;
-      if ((2 * i + 1 <= (int)heap_min.size() - 1) &&
-          (heap_min[2 * i + 1].val < heap_min[u].val)) {
-        u = 2 * i + 1;
-      }
-      if (heap_min[i].val > heap_min[u].val) {
-        ExchangeHeapMin(i, u);
-        i = u;
-      } else {
-        break;
-      }
-    }
+  void SiftDownHeapMin(int i);
+  void SiftDownHeapMax(int i);
+
+ public:
+  MinMax() {
+    heap_min_.resize(1);
+    heap_max_.resize(1);
   }
-  void SiftDownHeapMax(int i) {
-    while (2 * i <= (int)heap_max.size() - 1) {
-      int u = 2 * i;
-      if ((2 * i + 1 <= (int)heap_max.size() - 1) &&
-          (heap_max[2 * i + 1].val > heap_max[u].val)) {
-        u = 2 * i + 1;
-      }
-      if (heap_max[i].val < heap_max[u].val) {
-        ExchangeHeapMax(i, u);
-        i = u;
-      } else {
-        break;
-      }
-    }
-  }
-  void Insert(int x) {
-    heap_min.push_back(Element(x, heap_min.size()));
-    heap_max.push_back(Element(x, heap_max.size()));
-    SiftUpHeapMin((int)heap_min.size() - 1);
-    SiftUpHeapMax((int)heap_max.size() - 1);
-  }
-  int GetMin() { return heap_min[1].val; }
-  int GetMax() { return heap_max[1].val; }
-  void EraseHeapMin(int i) {
-    ExchangeHeapMin(i, (int)heap_min.size() - 1);
-    heap_min.pop_back();
-    SiftUpHeapMin(i);
-    SiftDownHeapMin(i);
-  }
-  void EraseHeapMax(int i) {
-    ExchangeHeapMax(i, (int)heap_max.size() - 1);
-    heap_max.pop_back();
-    SiftUpHeapMax(i);
-    SiftDownHeapMax(i);
-  }
+  void Insert(int x);
+  int GetMin() { return heap_min_[1].val; }
+  int GetMax() { return heap_max_[1].val; }
   void ExtractMin() {
-    EraseHeapMax(heap_min[1].idx);
+    EraseHeapMax(heap_min_[1].idx);
     EraseHeapMin(1);
   }
   void ExtractMax() {
-    EraseHeapMin(heap_max[1].idx);
+    EraseHeapMin(heap_max_[1].idx);
     EraseHeapMax(1);
   }
-  int Size() { return (int)heap_min.size() - 1; }
+  size_t Size() { return heap_min_.size() - 1; }
   void Clear() {
-    heap_min.resize(1);
-    heap_max.resize(1);
+    heap_min_.resize(1);
+    heap_max_.resize(1);
   }
 };
 
@@ -148,4 +104,59 @@ int main() {
       std::cout << "ok\n";
     }
   }
+}
+
+void MinMax::SiftDownHeapMin(int i) {
+  int sz = heap_min_.size();
+  while (2 * i <= sz - 1) {
+    int u = 2 * i;
+    if ((2 * i + 1 <= sz - 1) &&
+        (heap_min_[2 * i + 1].val < heap_min_[u].val)) {
+      u = 2 * i + 1;
+    }
+    if (heap_min_[i].val > heap_min_[u].val) {
+      ExchangeHeapMin(i, u);
+      i = u;
+    } else {
+      break;
+    }
+  }
+}
+
+void MinMax::SiftDownHeapMax(int i) {
+  int sz = heap_max_.size();
+  while (2 * i <= sz - 1) {
+    int u = 2 * i;
+    if ((2 * i + 1 <= sz - 1) &&
+        (heap_max_[2 * i + 1].val > heap_max_[u].val)) {
+      u = 2 * i + 1;
+    }
+    if (heap_max_[i].val < heap_max_[u].val) {
+      ExchangeHeapMax(i, u);
+      i = u;
+    } else {
+      break;
+    }
+  }
+}
+
+void MinMax::Insert(int x) {
+  heap_min_.push_back(Element(x, heap_min_.size()));
+  heap_max_.push_back(Element(x, heap_max_.size()));
+  SiftUpHeapMin(heap_min_.size() - 1);
+  SiftUpHeapMax(heap_max_.size() - 1);
+}
+
+void MinMax::EraseHeapMin(int i) {
+  ExchangeHeapMin(i, heap_min_.size() - 1);
+  heap_min_.pop_back();
+  SiftUpHeapMin(i);
+  SiftDownHeapMin(i);
+}
+
+void MinMax::EraseHeapMax(int i) {
+  ExchangeHeapMax(i, heap_max_.size() - 1);
+  heap_max_.pop_back();
+  SiftUpHeapMax(i);
+  SiftDownHeapMax(i);
 }
