ee1c5a165b8971a4f0613419e12f26c90d8f0c0a
VasVol <111646443+VasVol@users.noreply.github.com>
2022-12-18 21:19:43 +0300

    Create taskD.cpp

---
+++ 5contest/taskD.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <random>
+
+std::random_device rd;
+std::mt19937 mt(rd());
+std::uniform_real_distribution<double> dist(1.0, 10.0);
+
+struct Node {
+  long long x;
+  double y;
+  int count = 1;
+  Node* right = nullptr;
+  Node* left = nullptr;
+  Node(long long x, double y) : x(x), y(y) {}
+};
+
+void Update(Node* node) {
+  if (node != nullptr) {
+    node->count = (node->left != nullptr ? node->left->count : 0) +
+                  (node->right != nullptr ? node->right->count : 0) + 1;
+  }
+}
+
+std::pair<Node*, Node*> Split(Node* node, long long k) {
+  if (node == nullptr) {
+    return {nullptr, nullptr};
+  }
+  if (node->x <= k) {
+    std::pair p = Split(node->right, k);
+    node->right = p.first;
+    Update(node);
+    return {node, p.second};
+  }
+  std::pair p = Split(node->left, k);
+  node->left = p.second;
+  Update(node);
+  return {p.first, node};
+}
+
+Node* Merge(Node* a, Node* b) {
+  if (a == nullptr) {
+    return b;
+  }
+  if (b == nullptr) {
+    return a;
+  }
+  if (a->y < b->y) {
+    a->right = Merge(a->right, b);
+    Update(a);
+    return a;
+  }
+  b->left = Merge(a, b->left);
+  Update(b);
+  return b;
+}
+
+class Treap {
+ private:
+  Node* root_ = nullptr;
+  long long Next(long long x, Node* node);
+  long long Prev(long long x, Node* node);
+  long long Kth(int k, Node* node);
+  long long Sum(long long l, long long r, Node* node);
+
+ public:
+  void Insert(long long x);
+  void Erase(long long x);
+  bool Find(long long x);
+  long long Next(long long x);
+  long long Prev(long long x);
+  long long Kth(int k);
+  long long Sum(long long l, long long r);
+  void Clear(Node* curr);
+  ~Treap();
+};
+
+Treap::~Treap() { Clear(root_); }
+
+long long Treap::Next(long long x) { return Next(x, root_); }
+
+long long Treap::Kth(int k) { return Kth(k, root_); }
+
+long long Treap::Kth(int k, Node* node) {
+  if (node == nullptr || k < 0 || k >= node->count) {
+    return -1;
+  }
+  int i = node->left != nullptr ? node->left->count : 0;
+  if (i > k) {
+    return Kth(k, node->left);
+  }
+  if (i == k) {
+    return node->x;
+  }
+  return Kth(k - i - 1, node->right);
+}
+
+long long Treap::Prev(long long x) { return Prev(x, root_); }
+
+long long Treap::Next(long long x, Node* node) {
+  if (node == nullptr) {
+    return -1;
+  }
+  if (node->x <= x) {
+    return Next(x, node->right);
+  }
+  long long ans = Next(x, node->left);
+  if (ans != -1) {
+    return ans;
+  }
+  return node->x;
+}
+
+long long Treap::Prev(long long x, Node* node) {
+  if (node == nullptr) {
+    return -1;
+  }
+  if (node->x >= x) {
+    return Prev(x, node->left);
+  }
+  long long ans = Prev(x, node->right);
+  if (ans != -1) {
+    return ans;
+  }
+  return node->x;
+}
+
+void Treap::Insert(long long x) {
+  if (Find(x)) {
+    return;
+  }
+  std::pair p = Split(root_, x);
+  Node* new_element = new Node(x, dist(mt));
+  if (root_ == nullptr) {
+    root_ = new_element;
+  } else {
+    Node* tmp = Merge(p.first, new_element);
+    root_ = Merge(tmp, p.second);
+  }
+  Update(root_);
+}
+
+void Treap::Erase(long long x) {
+  if (!Find(x)) {
+    return;
+  }
+  std::pair p = Split(root_, x);
+  std::pair tmp = Split(p.first, x - 1);
+  root_ = Merge(tmp.first, p.second);
+  Update(root_);
+  delete tmp.second;
+}
+
+bool Treap::Find(long long x) {
+  Node* curr = root_;
+  while (curr != nullptr) {
+    if (curr->x == x) {
+      return true;
+    }
+    if (curr->x > x) {
+      curr = curr->left;
+    } else {
+      curr = curr->right;
+    }
+  }
+  return false;
+}
+
+void Treap::Clear(Node* curr) {
+  if (curr != nullptr) {
+    Clear(curr->left);
+    Clear(curr->right);
+    delete curr;
+  }
+}
+
+long long Treap::Sum(long long l, long long r, Node* node) {
+  if (node == nullptr) {
+    return 0;
+  }
+  if (node->x > r) {
+    return Sum(l, r, node->left);
+  }
+  if (node->x < l) {
+    return Sum(l, r, node->right);
+  }
+  return Sum(l, r, node->left) + Sum(l, r, node->right);
+}
+long long Treap::Sum(long long l, long long r) { return Sum(l, r, root_); }
+
+int main() {
+  Treap t;
+  std::string s;
+  long long x;
+  while (std::cin >> s >> x) {
+    if (s == "insert") {
+      t.Insert(x);
+    } else if (s == "delete") {
+      t.Erase(x);
+    } else if (s == "exists") {
+      std::cout << (t.Find(x) ? "true" : "false") << "\n";
+    } else if (s == "next") {
+      long long ans = t.Next(x);
+      if (ans != -1) {
+        std::cout << ans << "\n";
+      } else {
+        std::cout << "none\n";
+      }
+    } else if (s == "prev") {
+      long long ans = t.Prev(x);
+      if (ans != -1) {
+        std::cout << ans << "\n";
+      } else {
+        std::cout << "none\n";
+      }
+    } else if (s == "kth") {
+      long long ans = t.Kth(x);
+      if (ans != -1) {
+        std::cout << ans << "\n";
+      } else {
+        std::cout << "none\n";
+      }
+    }
+  }
+}
