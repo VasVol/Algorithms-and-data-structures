f8c5093c9317fb255fc2eb58eda754bc9215bffc
VasVol <111646443+VasVol@users.noreply.github.com>
2023-04-04 13:10:57 +0300

    Update taskD.cpp

--- 6contest/taskD.cpp
+++ 6contest/taskD.cpp
@@ -3,23 +3,23 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  int n, k;
-  std::cin >> n >> k;
+int MinimumNumberOfExperiments(int n, int k) {
   if (k == 0 && n > 1) {
-    std::cout << -1;
-    return 0;
+    return -1;
   }
-  if (k > std::log2(n) + 10) {
+  if (k > std::log2(n)) {
     int p = 1;
     int ans = 0;
     while (p < n) {
       p *= 2;
       ++ans;
     }
-    std::cout << ans;
-    return 0;
+    return ans;
   }
+  // Пусть dp[i][j] - максимальная высота здания, которую можно
+  // узнать, имея i самолетиков и j попыток.
+  // Будем зранить dp[i] как dp1 и dp[i-1] как dp[0], потому что
+  // для пересчета dp[i] нужен только dp[i-1].
   std::vector<int> dp0(n + 1, 0);
   std::vector<int> dp1(n + 1, 0);
   for (int i = 1; i <= k; ++i) {
@@ -30,8 +30,14 @@ int main() {
   }
   for (int j = 0; j <= n; ++j) {
     if (dp0[j] >= n - 1) {
-      std::cout << j;
-      return 0;
+      return j;
     }
   }
+  return 0;
+}
+
+int main() {
+  int n, k;
+  std::cin >> n >> k;
+  std::cout << MinimumNumberOfExperiments(n, k);
 }
