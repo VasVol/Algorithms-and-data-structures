377fa6c509ceefde49e328495a7cf13d6418b703
Vasily Volovich <vasily.volovich@gmail.com>
2023-05-07 19:28:43 +0300

    Revert "Update 8constest_taskA.cpp"
    This reverts commit 1a24e12316667a5ae515cadc8dacba0dd3b15837.

--- 8contest/8constest_taskA.cpp
+++ 8contest/8constest_taskA.cpp
@@ -1,93 +1,84 @@
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <vector>
 
-void Fast() {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
-}
+class Graph {
+ private:
+  struct Edge {
+    long long num;
+    long long cost;
+  };
+  struct Vertex {
+    long long num;
+    long long dist;
+    bool operator<(const Vertex& other) const {
+      if (dist != other.dist) {
+        return dist < other.dist;
+      }
+      return num < other.num;
+    }
+  };
+  const long long kInf = 2009000999;
+  long long n_ = 0;
+  long long m_ = 0;
+  std::vector<std::vector<Edge>> g_;
+  std::vector<long long> min_dist_;
+  std::set<Vertex> not_used_;
+  std::vector<bool> used_;
 
-struct Elem {
-  int vertex;
-  int cost;
+ public:
+  Graph();
+  void Dijkstra(long long start);
 };
 
-void PrintAns(const std::vector<int>& ans, Elem elem) {
-  std::vector<int> answer;
-  answer.push_back(elem.vertex);
-  for (auto it = ans.rbegin(); it != ans.rend(); ++it) {
-    answer.push_back(*it);
-    if (*it == elem.vertex) {
-      break;
-    }
-  }
-  std::cout << answer.size() << "\n";
-  for (int x : answer) {
-    std::cout << x + 1 << " ";
-  }
-}
-
-void FordBellman(int num, std::vector<std::vector<int>>& dp,
-                 std::vector<std::vector<Elem>>& parent,
-                 const std::vector<std::vector<int>>& costs) {
-  const int kNoEdge = 1e5;
-  for (int cost = 1; cost <= num; ++cost) {
-    for (int vertex = 0; vertex < num; ++vertex) {
-      dp[vertex][cost] = dp[vertex][cost - 1];
-      parent[vertex][cost] = parent[vertex][cost - 1];
-      for (int from = 0; from < num; ++from) {
-        if (costs[from][vertex] == kNoEdge) {
-          continue;
-        }
-        if (dp[from][cost - 1] + costs[from][vertex] < dp[vertex][cost]) {
-          dp[vertex][cost] = dp[from][cost - 1] + costs[from][vertex];
-          parent[vertex][cost] = {from, cost - 1};
-        }
-      }
-    }
-  }
-}
-
-void Solve(int num, const std::vector<std::vector<int>>& costs) {
-  const int kInf = 1e9;
-  std::vector<std::vector<int>> dp(num, std::vector<int>(num + 1, kInf));
-  dp[0][0] = 0;
-  std::vector<std::vector<Elem>> parent(num,
-                                        std::vector<Elem>(num + 1, {-1, -1}));
-  FordBellman(num, dp, parent, costs);
-  int curr = -1;
-  for (int vertex = 0; vertex < num; ++vertex) {
-    if (dp[vertex][num] < dp[vertex][num - 1]) {
-      curr = vertex;
-      break;
-    }
-  }
-  if (curr == -1) {
-    std::cout << "NO\n";
-    return;
-  }
-  std::cout << "YES\n";
-  std::vector<int> ans;
-  std::vector<bool> used(num, false);
-  Elem elem = {curr, num};
-  while (!used[elem.vertex]) {
-    ans.push_back(elem.vertex);
-    used[elem.vertex] = true;
-    elem = parent[elem.vertex][elem.cost];
-  }
-  PrintAns(ans, elem);
-}
-
 int main() {
-  Fast();
-  int num;
-  std::cin >> num;
-  std::vector<std::vector<int>> costs(num, std::vector<int>(num));
-  for (int i = 0; i < num; ++i) {
-    for (int j = 0; j < num; ++j) {
-      std::cin >> costs[i][j];
+  long long cnt;
+  std::cin >> cnt;
+  for (long long i = 0; i < cnt; ++i) {
+    Graph gr;
+    long long start;
+    std::cin >> start;
+    gr.Dijkstra(start);
+  }
+}
+
+Graph::Graph() {
+  std::cin >> n_ >> m_;
+  g_.resize(n_);
+  min_dist_.resize(n_, kInf);
+  used_.resize(n_, false);
+  not_used_ = std::set<Vertex>();
+  for (long long i = 0; i < m_; ++i) {
+    long long v1;
+    long long v2;
+    long long cost;
+    std::cin >> v1 >> v2 >> cost;
+    g_[v1].push_back({v2, cost});
+    g_[v2].push_back({v1, cost});
+  }
+}
+
+void Graph::Dijkstra(long long start) {
+  min_dist_[start] = 0;
+  not_used_.insert({start, min_dist_[start]});
+  while (!not_used_.empty()) {
+    auto it = not_used_.begin();
+    auto vert = *it;
+    used_[vert.num] = true;
+    not_used_.erase(it);
+    for (auto edge : g_[vert.num]) {
+      if (used_[edge.num]) {
+        continue;
+      }
+      long long new_dist =
+          std::min(min_dist_[edge.num], min_dist_[vert.num] + edge.cost);
+      not_used_.erase({edge.num, min_dist_[edge.num]});
+      min_dist_[edge.num] = new_dist;
+      not_used_.insert({edge.num, min_dist_[edge.num]});
     }
   }
-  Solve(num, costs);
+  for (auto dist : min_dist_) {
+    std::cout << dist << " ";
+  }
 }
