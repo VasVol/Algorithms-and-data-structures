6e99a63ac498773cd10c1c982406f09b975f5290
VasVol <111646443+VasVol@users.noreply.github.com>
2023-05-15 17:47:32 +0300

    Update taskB.cpp

--- 9contest/taskB.cpp
+++ 9contest/taskB.cpp
@@ -3,43 +3,17 @@
 #include <algorithm>
 
 // Disjoint Set Union
-struct DSU {
+class DSU {
+ private:
   std::vector<int> parent;
   std::vector<int> size;
-  DSU(int n) {
-    parent.resize(n, -1);
-    size.resize(n, 1);
-  }
-  int get(int v) {
-    if (parent[v] == -1) {
-      return v;
-    }
-    int x = get(parent[v]);
-    parent[v] = x;
-    return x;
-  }
-  void unite(int u, int v) {
-    u = get(u);
-    v = get(v);
-    if (u == v) {
-      return;
-    }
-    if (size[u] < size[v]) {
-      std::swap(u, v);
-    }
-    parent[v] = u;
-    size[u] += size[v];
-  }
-  bool same(int u, int v) {
-    return get(u) == get(v);
-  }
-};
 
-void Fast() {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
-}
+ public:
+  DSU(int n);
+  int get(int v);
+  void unite(int u, int v);
+  bool same(int u, int v);
+};
 
 struct Edge {
   int u;
@@ -57,14 +31,7 @@ bool operator<(const Edge& a, const Edge& b) {
   return a.v < b.v;
 }
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  std::vector<Edge> g(m);
-  for (int i = 0; i < m; ++i) {
-    std::cin >> g[i].u >> g[i].v >> g[i].c;
-    --g[i].u, --g[i].v;
-  }
+void solve(int n, std::vector<Edge>& g) {
   std::sort(g.begin(), g.end());
   long long ans = 0;
   DSU dsu(n);
@@ -75,4 +42,46 @@ int main() {
     }
   }
   std::cout << ans;
+}
+
+int main() {
+  int n, m;
+  std::cin >> n >> m;
+  std::vector<Edge> g(m);
+  for (int i = 0; i < m; ++i) {
+    std::cin >> g[i].u >> g[i].v >> g[i].c;
+    --g[i].u, --g[i].v;
+  }
+  solve(n, g);
+}
+
+int DSU::get(int v) {
+  if (parent[v] == -1) {
+    return v;
+  }
+  int x = get(parent[v]);
+  parent[v] = x;
+  return x;
+}
+
+void DSU::unite(int u, int v) {
+  u = get(u);
+  v = get(v);
+  if (u == v) {
+    return;
+  }
+  if (size[u] < size[v]) {
+    std::swap(u, v);
+  }
+  parent[v] = u;
+  size[u] += size[v];
+}
+
+bool DSU::same(int u, int v) {
+  return get(u) == get(v);
+}
+
+DSU::DSU(int n) {
+  parent.resize(n, -1);
+  size.resize(n, 1);
 }
